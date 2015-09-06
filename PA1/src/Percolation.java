e2fb7b801ee2f2bad666b054b4119f13a367ee37
Dhawan Gayash <gitresumeuser@gmail.com>
2015-09-06 01:58:47 -0700

    adding Percolation stub files

---
+++ PA1/src/Percolation.java
@@ -0,0 +1,28 @@
+/**
+ * Created by dgayash on 9/6/15.
+ */
+public class Percolation {
+    public Percolation(int N) {
+
+    }
+
+    public void open(int i, int j) {
+
+    }
+
+    public boolean isOpen(int i, int j) {
+        return false;
+    }
+
+    public boolean isFull(int i, int j) {
+        return false;
+    }
+
+    public boolean percolates() {
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ PA1/src/PercolationStats.java
@@ -0,0 +1,27 @@
+/**
+ * Created by dgayash on 9/6/15.
+ */
+public class PercolationStats {
+    public PercolationStats(int N, int T) {
+    }
+
+    public double mean() {
+        throw new UnsupportedOperationException("Need to be implemented");
+    }
+
+    public double stddev() {
+        throw new UnsupportedOperationException("Need to be implemented");
+    }
+
+    public double confidenceLo() {
+        throw new UnsupportedOperationException("Need to be implemented");
+    }
+
+    public double confidenceHi() {
+        throw new UnsupportedOperationException("Need to be implemented");
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
