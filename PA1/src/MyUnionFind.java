dc19d619015983122edc24511c712ab4f5c8e51e
Dhawan Gayash <gitresumeuser@gmail.com>
2015-09-06 03:36:00 -0700

    Adding simple implementation of union find

---
+++ PA1/src/MyUnionFind.java
@@ -0,0 +1,58 @@
+import edu.princeton.cs.algs4.SET;
+
+/**
+ * Created on 9/6/15.
+ */
+public class MyUnionFind {
+
+    int[] arr;
+    MyUnionFind(int N) {
+        arr = new int[N];
+        for (int i =0; i < arr.length; i++) {
+            arr[i] = i;
+        }
+    }
+
+    boolean isConnected(int p, int q) {
+        return find(p) == find(q);
+    }
+
+    void union(int p, int q) {
+        int root_q = find(q);
+        int root_p = find(p);
+        arr[root_p] = arr[root_q];
+    }
+
+    int find(int p) {
+        while(arr[p] != p) {
+            p = arr[p];
+        }
+        return p;
+    }
+
+    int count() {
+        SET<Integer> roots = new SET<Integer>();
+        for (int i = 0;i < arr.length; i++) {
+            int root = find(i);
+            if (!roots.contains(root)) {
+                roots.add(root);
+            }
+        }
+        return roots.size();
+    }
+
+    public String toString() {
+        StringBuffer sb = new StringBuffer();
+        sb.append("|");
+        for(int i =0; i<arr.length; i++)
+            sb.append(" " + i + " | " );
+        sb.append("\n" + "|");
+        for(int i =0; i<arr.length; i++)
+            sb.append("------");
+        sb.append("\n" + "|");
+        for(int i =0; i<arr.length; i++)
+            sb.append(" " + arr[i] + " | " );
+
+        return sb.append("\n").toString();
+    }
+}
---
+++ PA1/src/MyUnionFindTest.java
@@ -0,0 +1,41 @@
+/**
+ * Created on 9/6/15.
+ */
+public class MyUnionFindTest {
+    public static void main(String[] args) {
+//        testNumbers();
+        testQuiz();
+    }
+
+    static void testQuiz() {
+        MyUnionFind unionFind = new MyUnionFind(10);
+        unionFind.union(1, 2);
+        unionFind.union(3, 4);
+        unionFind.union(5, 6);
+        unionFind.union(7, 8);
+        unionFind.union(7, 9);
+        unionFind.union(2, 8);
+        unionFind.union(0, 5);
+        unionFind.union(1, 9);
+        System.out.println(unionFind);
+        System.out.println("Number of Connected Groups: " + unionFind.count());
+    }
+
+    static void testNumbers(){
+        MyUnionFind unionFind = new MyUnionFind(10);
+        unionFind.union(4, 3);
+        unionFind.union(3, 8);
+        unionFind.union(6, 5);
+        unionFind.union(9, 4);
+        unionFind.union(2, 1);
+        unionFind.union(8, 9);
+        unionFind.union(5, 0);
+        unionFind.union(7, 2);
+        unionFind.union(6, 1);
+        unionFind.union(1, 0);
+        unionFind.union(6, 7);
+        System.out.println(unionFind.isConnected(1, 3));
+        System.out.println(unionFind);
+        System.out.println("Number of Connected Groups: " + unionFind.count());
+    }
+}
