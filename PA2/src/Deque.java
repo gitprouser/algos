8203a4991db8738ba33f3724f7b28a930884ef29
Dhawan Gayash <gitresumeuser@gmail.com>
2016-02-24 13:14:59 -0800

    PA2 added

---
+++ PA2/src/Deque.java
@@ -0,0 +1,74 @@
+import java.util.Iterator;
+
+public class Deque<T> implements Iterable<T> {
+
+    /**
+     * Construct an empty deque.
+     */
+    public Deque() {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    /**
+     * Is the deque empty?
+     * @return
+     */
+    public boolean isEmpty() {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    /**
+     * Return the number of items on the deque.
+     *
+     * @return The number of items of the deque.
+     */
+    public int size() {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    /**
+     * Add the item to the front.
+     * @return
+     */
+    public void addFirst(T item) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    /**
+     * Add the item to the end.
+     * @param item
+     */
+    public void addLast (T item) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+
+    /**
+     * Remove and return the item from the front.
+     * @return
+     */
+    public T removeFirst() {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    /**
+     * Remove and return the last item from the end.
+     * @return
+     */
+    public T removeLast() {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    /**
+     * Return an iterator over items in order from front to end.
+     * @return
+     */
+    public Iterator<T> iterator() {
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ PA2/src/Subset.java
@@ -0,0 +1,5 @@
+/**
+ * Created on 2/23/16.
+ */
+public class Subset {
+}
